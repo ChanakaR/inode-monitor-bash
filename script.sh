73acda8d18f1cc8baf01396ab3f65ccef377ea98
ChanakaR <bmcrathnayaka@gmail.com>
2018-11-08 12:17:26 +0530

    added cleanup mail

--- script.sh
+++ script.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-CURRENT=$(df -ih / | grep / | awk '{ print $5}' | sed 's/%//g')
+CURRENT=$(df -ih /tmp | grep /tmp | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=10
+ALERTED=0
 
-echo "$CURRENT"
+echo $CURRENT
 
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    mail -s 'Disk Space Alert' example@example.com << EOF
-Your root partition remaining free space is critically low. Used: $CURRENT%
+if [[ $CURRENT -gt $THRESHOLD ]]
+ then
+   if [[ALERTED -eq 0]]
+     then
+       mail -s 'Artemis - INode usage Alert' example@example.com << EOF
+   tmp partition remaining free INodes value is critically low. Used: $CURRENT%
 EOF
+       ALERTED=1
+   fi
+ else
+   if [[ALERTED -eq 1]]
+     then
+       mail -s 'Artemis - INode usage Alert Cleared' example@example.com << EOF
+   tmp partition remaining free INodes usage alert has been cleared. Used: $CURRENT%
+EOF
+       ALERTED=0
+   fi
 fi
