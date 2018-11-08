bbdd38e9bba41e852f4445766ee6d03cc1f23e0a
ChanakaR <bmcrathnayaka@gmail.com>
2018-11-08 13:25:34 +0530

    fine tuned the functionality and improved code

--- script.sh
+++ script.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-CURRENT=$(df -ih /tmp | grep /tmp | awk '{ print $5}' | sed 's/%//g')
-THRESHOLD=10
-ALERTED=0
+current=$(df -ih /tmp | grep / | awk '{ print $5}' | sed 's/%//g')
+threshold=90
+alerted=0
 
-echo $CURRENT
-
-if [[ $CURRENT -gt $THRESHOLD ]]
- then
-   if [[ALERTED -eq 0]]
-     then
-       mail -s 'Artemis - INode usage Alert' example@example.com << EOF
-   tmp partition remaining free INodes value is critically low. Used: $CURRENT%
-EOF
-       ALERTED=1
-   fi
- else
-   if [[ALERTED -eq 1]]
-     then
-       mail -s 'Artemis - INode usage Alert Cleared' example@example.com << EOF
-   tmp partition remaining free INodes usage alert has been cleared. Used: $CURRENT%
-EOF
-       ALERTED=0
-   fi
+if [[ "$current" -gt "$threshold" && "$alerted" -eq "0" ]] ; then
+    alerted=1
+    cat mail_header.txt > mail_body.txt
+    echo "Subject: INode Usage Alert" >> mail_body.txt
+    echo "INode usage of /tmp directory is critically high. Please run 'df -ih /tmp' to see the usage summary" >> mail_body.txt
+else
+    if [[ "$alerted" -eq "1" ]] ; then
+        alerted=0
+        cat mail_header.txt > mail_body.txt
+        echo "Subject: INode Usage Alert Cleared" >> mail_body.txt
+        echo "INode high usage alert has been cleared. Please run 'df -ih /tmp' to see the usage summary" >> mail_body.txt
+    fi
 fi
